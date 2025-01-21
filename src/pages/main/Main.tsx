ecbb031eeb52f042bd0a461f639edfa111e20626
jicadz <cadiz.joshuaiangit2011@gmail.com>
2025-01-21 21:06:53 +0800

    second commit

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react"
+import React from "react"
 import { FloatingDock } from "../../../components/ui/floating-dock";
 import Image from "next/image";
 import AppStoreIcon from "../../../public/AppStore.png"
